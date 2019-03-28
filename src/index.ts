c06e03c363730b56412ecbd9f5f03c4d49ba62e9
Franck Ledoux <franck.ledoux.pro@gmail.com>
2019-03-28 22:41:53 +0100

    first steps

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+export interface RetryOptions {
+  maxTry: number;
+  delay: number;
+}
+
+export async function retry<T>(fn: () => T, retryOptions: RetryOptions) {
+  const wrapped = () => new Promise((resolve, reject) => {
+    try {
+      resolve(fn());
+    } catch (err) {
+      reject(err);
+    }
+  });
+  return retryAsync(wrapped, retryOptions);
+}
+
+export async function retryAsync<T>(fn: () => Promise<T>, { maxTry, delay }: RetryOptions): Promise<T> {
+  try {
+    return await fn();
+  } catch (err) {
+    if (maxTry > 1) {
+      await wait(delay);
+      return await retryAsync(fn, { delay, maxTry: maxTry-- });
+    }
+    throw err;
+  }
+}
+
+function wait(duration: number) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+}
---
+++ test/retry.ts
@@ -0,0 +1,3 @@
+describe('Retry async', () => {
+  it('work the first time,no retry', () => { });
+})
