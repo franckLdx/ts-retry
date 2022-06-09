ec628863de35cfd9c3580dfd0bbee33228e912e7
Utilisateur <franck.ledoux.pro@gmail.com>
2022-06-09 22:28:19 +0200

    updgrade deps

--- lib/cjs/retry/utils/options.d.ts
+++ lib/cjs/retry/utils/options.d.ts
@@ -1,3 +1,3 @@
 import { RetryOptions, UNTIL } from "../options";
 export declare type RetryUtilsOptions = Exclude<RetryOptions<void>, "until">;
-export declare const retryUntilOptionsToRetryOptionsHof: <RETURN_TYPE>(until: UNTIL<RETURN_TYPE>) => (retryOptions?: RetryOptions<void> | undefined) => RetryOptions<RETURN_TYPE>;
+export declare const retryUntilOptionsToRetryOptionsHof: <RETURN_TYPE>(until: UNTIL<RETURN_TYPE>) => (retryOptions?: RetryUtilsOptions) => RetryOptions<RETURN_TYPE>;
