5ea29c7b34f1681f0d1e98ec338c0e4b694602ff
Utilisateur <franck.ledoux.pro@gmail.com>
2022-10-13 22:00:55 +0200

    add last result in ToomanyRetries

--- src/retry/tooManyTries.ts
+++ src/retry/tooManyTries.ts
@@ -1,10 +1,14 @@
-export class TooManyTries extends Error {
-  constructor() {
+export class TooManyTries<RETURN_TYPE> extends Error {
+  constructor(private lastResult: RETURN_TYPE | undefined = undefined) {
     super("function did not complete within allowed number of attempts");
   }
   tooManyTries = true;
+
+  public getLastResult(): RETURN_TYPE | undefined {
+    return this.lastResult;
+  }
 }
 
-export function isTooManyTries(error: unknown): error is TooManyTries {
-  return (error as TooManyTries).tooManyTries === true;
+export function isTooManyTries(error: unknown): error is TooManyTries<any> {
+  return (error as TooManyTries<any>).tooManyTries === true;
 }
